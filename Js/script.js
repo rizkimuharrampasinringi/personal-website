89e28124ef7fac28c6c4b4fdc63a7d6c03fd24fa
Rizki Muh Pasinringi <100172120+Rizki-MP@users.noreply.github.com>
2022-05-31 20:42:41 +0800

    Update section header

---
+++ Js/script.js
@@ -0,0 +1,33 @@
+// Navbar Fixed 
+ window.onscroll = function () { 
+   const header = document.querySelector('header'); 
+   const fixedNav = header.offsetTop; 
+   const toTop = document.querySelector('#to-top'); 
+  
+   if (window.pageYOffset > fixedNav) { 
+     header.classList.add('navbar-fixed'); 
+     toTop.classList.remove('hidden'); 
+     toTop.classList.add('flex'); 
+   } else { 
+     header.classList.remove('navbar-fixed'); 
+     toTop.classList.remove('flex'); 
+     toTop.classList.add('hidden'); 
+   } 
+ }; 
+  
+ // Hamburger 
+ const hamburger = document.querySelector('#hamburger'); 
+ const navMenu = document.querySelector('#nav-menu'); 
+  
+ hamburger.addEventListener('click', function () { 
+   hamburger.classList.toggle('hamburger-active'); 
+   navMenu.classList.toggle('hidden'); 
+ }); 
+  
+ // Klik di luar hamburger 
+ window.addEventListener('click', function (e) { 
+   if (e.target != hamburger && e.target != navMenu) { 
+     hamburger.classList.remove('hamburger-active'); 
+     navMenu.classList.add('hidden'); 
+   } 
+ });
